1f6e7de8aa0a677f530c587bf2fa6a483c3948e9
omarali <omaraliabdelaziz250@gmail.com>
2026-06-09 12:09:14 +0300

    Add seed data for Users, Editors, and Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.destroy_all
+Editor.destroy_all
+Post.destroy_all
+PostEditor.destroy_all
+
+u1 = User.create!(
+  name: "omar",
+  email: "omar@test.com"
+)
+
+u2 = User.create!(
+  name: "ali",
+  email: "ali@test.com"
+)
+
+e1 = Editor.create!(
+  name: "ahmed",
+  email: "ahmed@test.com"
+)
+
+e2 = Editor.create!(
+  name: "mohamed",
+  email: "mohamed@test.com"
+)
+
+p1 = Post.create!(
+  title: "First Post",
+  body: "First Post",
+  creator: u1
+)
+
+p2 = Post.create!(
+  title: "Second Post",
+  body: "Second Post",
+  creator: u2
+)
+
+PostEditor.create!(post: p1, editor: e1)
+PostEditor.create!(post: p1, editor: e2)
+PostEditor.create!(post: p2, editor: e1)
